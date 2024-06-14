8d089fea3c0c82a8dd7292055ee344da12f6549a
Trisout <81978700+trisout78@users.noreply.github.com>
2024-06-14 19:49:33 +0200

    Bugs Fix & Improvements

--- src/main/java/fr/etercube/sauvetageexpress/utils/ConvertSecondToMinutesAndSeconds.java
+++ src/main/java/fr/etercube/sauvetageexpress/utils/ConvertSecondToMinutesAndSeconds.java
@@ -4,8 +4,8 @@ public class ConvertSecondToMinutesAndSeconds {
     public static String convertSecondsToMinutesAndSeconds(int totalSeconds) {
         int minutes = totalSeconds / 60;
         int seconds = totalSeconds % 60;
-        String minuteString = (minutes <= 1) ? "minute" : "minutes";
-        String secondString = (seconds <= 1) ? "seconde" : "secondes";
+        String minuteString = (minutes < 2) ? "minute" : "minutes";
+        String secondString = (seconds < 2) ? "seconde" : "secondes";
         return String.format("%d %s %d %s", minutes, minuteString, seconds, secondString);
     }
 }
